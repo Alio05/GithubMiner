472b47889a1d68b8e5aa164a8094e7f29b23b13c
DavidBomba <david.bomba.bastos@gmail.com>
2025-05-10 20:13:01 +0200

    He borrao una tonteria, eso si, TODO: que el post devuelva 201 no 200 que eso es get watafac

--- src/main/java/aiss/githubminer/controller/GitHubController.java
+++ src/main/java/aiss/githubminer/controller/GitHubController.java
@@ -19,20 +19,19 @@ public class GitHubController {
             "http://localhost:8080/gitminer/projects";
     @GetMapping("/{owner}/{repo}")
     public Project getData(@PathVariable String owner, @PathVariable
-                           String repo, @RequestParam(defaultValue = "5", required = false) Integer sinceCommits,
-                           @RequestParam(defaultValue = "20", required = false) Integer
-                                   sinceIssues, @RequestParam(defaultValue = "2", required = false) Integer maxPages){
+                           String repo, @RequestParam(defaultValue = "5") Integer sinceCommits,
+                           @RequestParam(defaultValue = "20") Integer
+                                   sinceIssues, @RequestParam(defaultValue = "2") Integer maxPages){
         return service.allData(owner,repo, sinceCommits, sinceIssues,
                 maxPages);
     }
     @PostMapping("/{owner}/{repo}")
     public Project sendData(@PathVariable String owner,@PathVariable
-                            String repo, @RequestParam(defaultValue = "5", required = false) Integer sinceCommits,
-                            @RequestParam(defaultValue = "20", required = false) Integer
-                                    sinceIssues, @RequestParam(defaultValue = "2", required = false) Integer maxPages){
+                            String repo, @RequestParam(defaultValue = "5") Integer sinceCommits,
+                            @RequestParam(defaultValue = "20") Integer
+                                    sinceIssues, @RequestParam(defaultValue = "2") Integer maxPages){
         Project project= service.allData(owner,repo, sinceCommits,
                 sinceIssues, maxPages);
-        System.out.println("JSON enviado a GitMiner:\n" + project.toString());
 
         HttpEntity<Project> request = new HttpEntity<>(project);
         ResponseEntity<Project> response =
